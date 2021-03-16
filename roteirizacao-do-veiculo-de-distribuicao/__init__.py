fdb1d92c5aff6fdbf5e5b781c4480e87e57cac4a
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2021-03-15 21:56:29 -0300

    - Updated script

--- roteirizacao-do-veiculo-de-distribuicao/__init__.py
+++ roteirizacao-do-veiculo-de-distribuicao/__init__.py
@@ -17,10 +17,44 @@ class Coordinate(object):
 
 
 class HealthUnit:
-    def __init__(self, coordinate, wasVisited):
+    def __init__(self, name, coordinate, was_visited):
+        self.name = name
         self.coordinate = coordinate
+        self.was_visited = was_visited
+
+    def __str__(self):
+        return "(" + self.name + " - " + self.coordinate.__str__() + ", was_visited: " + ("True" if self.was_visited else "False") + ")"
+
+    def linearDistanceTo(self, other):
+        return self.coordinate.linearDistanceTo(other.coordinate)
 
 
 class State:
     def __init__(self, healthUnits):
         self.healthUnits = healthUnits
+
+    def __str__(self):
+        out = ""
+        for i in self.healthUnits:
+            out += i.__str__() + "\n"
+        return out
+
+
+us_01 = HealthUnit("US1", Coordinate(0, 0), False)
+us_02 = HealthUnit("US2", Coordinate(2, 0), False)
+us_03 = HealthUnit("US3", Coordinate(1, 1), False)
+us_04 = HealthUnit("US4", Coordinate(2, 0), False)
+us_05 = HealthUnit("US5", Coordinate(2, 2), False)
+
+
+initial_state = State([
+    us_01,
+    us_02,
+    us_03,
+    us_04,
+    us_05
+])
+
+# print(initial_state)
+
+print(us_01.linearDistanceTo(us_02))
