c8a5404cdcb706669efb0f307842c6ffe25ad956
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2021-05-12 09:12:38 -0300

    - Added content

--- simulation/benchmark.py
+++ simulation/benchmark.py
@@ -24,15 +24,14 @@ import matplotlib.pyplot as plt
 x = []
 y = []
 
-population_size = 100
-number_of_parents = 10
-probability_of_mutation = 0.1
+population_size = 20
+number_of_parents = 5
+probability_of_mutation = 0.01
 verbose = False
 
 origin, places = Place.from_file("places.txt")
 
-
-for number_of_rounds in range(10, 100, 2):
+for number_of_rounds in range(1, 1000, 1):
     population = GA.population(places, population_size, verbose)
 
     for i in range(number_of_rounds):
@@ -59,14 +58,10 @@ for number_of_rounds in range(10, 100, 2):
     y.append(GA.hs_fitness(route, None))
 
 
-# t = np.arange(0.0, 2.0, 0.01)
-# s = 1 + np.sin(2 * np.pi * t)
-
 fig, ax = plt.subplots()
 ax.plot(x, y)
 
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
+ax.set(xlabel='number of rounds', ylabel='distance')
 ax.grid()
 
 # fig.savefig("test.png")
